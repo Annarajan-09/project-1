703959923a037f079481fad82e1bc0f197222013
Annarajan-09 <87439660+Annarajan-09@users.noreply.github.com>
2022-09-03 14:25:25 +0530

    Update Systeminfo.ps1

--- Systeminfo.ps1
+++ Systeminfo.ps1
@@ -1,2 +1,3 @@
 ﻿#Get-WmiObject -Class win32_physicalmemory | Select-Object BankLabel, Capacity, Speed, PartNumber
 Write-Host "Hello World, I'm a Jenkins build!"
+Write-Host "Santhana Sree Lakshmi, I'm a Jenkins build!"
